e2f1f8edfe7cf99a58cf705b1df453395e278c79
knikoletic <karlo.nikoletic@verybigthings.com>
2021-12-31 11:44:15 +0100

    homepage

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [duct/module.logging "0.5.0"]
                  [duct/module.sql "0.6.1"]
                  [duct/module.web "0.7.3"]
-                 [org.xerial/sqlite-jdbc "3.34.0"]]
+                 [org.xerial/sqlite-jdbc "3.34.0"]
+                 [hiccup "1.0.5"]]
   :plugins [[duct/lein-duct "0.12.3"]]
   :main ^:skip-aot film-ratings.main
   :uberjar-name "film-ratings.jar"
